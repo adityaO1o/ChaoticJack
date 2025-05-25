844c1564b98d6bce24ee1ecddf56fe18d5959940
Aditya Sharma <aditya2562sharma@gmail.com>
2025-05-25 17:28:59 +0530

    final edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,47 @@
+import { useEffect } from "react";
+import { useLocation, Routes, Route } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import MarketingSolutions from "./pages/MarketingSolutions";
 import WhoWeAre from "./pages/WhoWeAre";
 import Work from "./pages/Work";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
-import ScrollToTop from "./components/ScrollToTop"; // <-- Import here
+import ScrollToTop from "./components/ScrollToTop";
+import ReactGA from "react-ga4";
+
+// Initialize GA only once (outside the component)
+ReactGA.initialize("G-PK2Q0QJ0NV");
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <ScrollToTop /> {/* <-- Add here */}
+const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/marketing-solutions" element={<MarketingSolutions />} />
           <Route path="/who-we-are" element={<WhoWeAre />} />
           <Route path="/work" element={<Work />} />
           <Route path="/contact" element={<Contact />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
